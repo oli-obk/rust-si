ae1cccaaf8d536dfac2876eacfe7ee9263d2e67b
Oliver Schneider <git1984941651981@oli-obk.de>
2015-04-16 18:12:07 +0200

    a generic read function for all FromStr types

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
-use std::io::BufRead;
+use std::io::{BufRead, Read};
+use std::str::{FromStr, from_utf8};
+use std::fmt::Debug;
 
 pub fn read_line() -> String {
     let stdin = std::io::stdin();
     let stdin = stdin.lock();
     let mut lines = stdin.lines();
     lines.next().unwrap().unwrap()
+}
+
+pub fn read<T: FromStr>() -> T
+    where <T as FromStr>::Err: Debug
+{
+    let stdin = std::io::stdin();
+    let stdin = stdin.lock();
+    let v: Vec<u8> = stdin.bytes()
+        .map(|c| c.unwrap())
+        .take_while(|c| b" \n\r\t".contains(&c))
+        .collect();
+    FromStr::from_str(from_utf8(&v).unwrap()).unwrap()
 }
