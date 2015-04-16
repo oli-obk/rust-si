36ad70b5aec1d380a00d2d706106d45e49ac5610
Oliver Schneider <git1984941651981@oli-obk.de>
2015-04-16 18:27:03 +0200

    some examples and documentation

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 ```rust
 let s: String = si::read_line();
+let i: i32 = si::read(); // read until a whitespace and try to get a number
+let word: String = si::read(); // read until a whitespace (but not including it)
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use std::io::{BufRead, Read};
 use std::str::{FromStr, from_utf8};
 use std::fmt::Debug;
 
+
+/// the `read_line` function returns all characters up to but not including the next `\n`
+/// ```rust
+/// let s: String = si::read_line();
+/// ```
 pub fn read_line() -> String {
     let stdin = std::io::stdin();
     let stdin = stdin.lock();
@@ -9,6 +14,12 @@ pub fn read_line() -> String {
     lines.next().unwrap().unwrap()
 }
 
+/// the `read` function reads until the next whitespace and tries to convert the read characters
+/// into the requested type `T` through the `FromStr` trait.
+/// ```rust
+/// let i: i32 = si::read(); // read until a whitespace and try to get a number
+/// let word: String = si::read(); // read until a whitespace (but not including it)
+/// ```
 pub fn read<T: FromStr>() -> T
     where <T as FromStr>::Err: Debug
 {
